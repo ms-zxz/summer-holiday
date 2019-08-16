50a655a9c39bb4f9da0979e2b162134e6fa183a5
ms-zxz <52478307+ms-zxz@users.noreply.github.com>
2019-08-16 12:06:23 +0800

    Delete led.h

--- led.h
+++
@@ -1,9 +0,0 @@
-#ifndef LED_H 
-#define LED_H 
-#include "sys.h"         //LED ¶Ë¿Ú¶¨Òå
-
-#define LED0 PBout(5)    // PB5
-#define LED1 PEout(5)    // PE5 
-void LED_Init(void);     //³õÊ¼»¯
-
-#endif
